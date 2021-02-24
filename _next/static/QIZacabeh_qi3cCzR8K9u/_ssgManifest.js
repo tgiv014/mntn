41eef2c4374d4cd661b4b9c011c219869c92caf2
tgiv014 <tgiv014@users.noreply.github.com>
2021-02-24 19:09:09 +0000

    deploy: ebf58432e896f414918b384d57c742416887a6dc

--- _next/static/QIZacabeh_qi3cCzR8K9u/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002F","\u002Fblog\u002F[postId]","\u002Fproject\u002F[projectId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/w0fzTjCq7F_1SFH6U1evu/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fcolophon","\u002F","\u002Fblog\u002F[postId]","\u002Fproject\u002F[projectId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
